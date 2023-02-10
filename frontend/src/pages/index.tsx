c240c293332b1658070cb741e9827c7709d25ffe
aya-se <kakkeno1@gmail.com>
2023-02-10 19:08:11 +0900

    Change frontend directory

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.scss'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Cloud Run SNS</h1>
+        <h1>Hello Cloud Run SNS</h1>
       </main>
     </>
   )

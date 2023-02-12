32f0b5ef66e743f83967c5c3b305e18f9e9e89b1
aya-se <kakkeno1@gmail.com>
2023-02-12 19:09:04 +0900

    Modify Card Design

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -10,13 +10,13 @@ export default function Card(props: CardProps) {
   return (
     <div className={styles.card}>
     <div className={styles.card_header}>
-      <Image className={styles.account_icon} width={100} height={100} src={props.account.image} alt="Account Icon" />
+      <Image className={styles.account_icon} width={50} height={50} src={props.account.image} alt="Account Icon" />
       <div className={styles.account_info}>
         <div className={styles.account_name}>{props.account.name}</div>
         <div className={styles.account_id}>@{props.account.id}</div>
       </div>
     </div>
-    <p className={styles.card_text}> {props.text} </p>
+    <div className={styles.card_text}> {props.text} </div>
   </div>
   );
 }

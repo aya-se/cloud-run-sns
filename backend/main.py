d42cea8c3dd4314c6562d1cb815f01ca3e2044b8
aya-se <kakkeno1@gmail.com>
2023-02-14 15:15:18 +0900

    Implement Connection to Cloud Firestore

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
+import os
 from fastapi import FastAPI
+from google.cloud import firestore
 
 app = FastAPI()
+db = firestore.Client(os.getenv("GOOGLE_CLOUD_PROJECT"))
 
 @app.get("/hello")
 def read_root(name: str):
-    return {'message': f'Hello {name}!'}
+    return {"message": f"Hello {name}!"}
 
 @app.post("/post")
 def post_root(text: str):
-    return {'message': f'OK {text}!'}
+    doc_ref = db.collection(u"posts").document()
+    doc_ref.set({
+        u"text": text
+    })
+    return {"message": f"OK {text}!"}

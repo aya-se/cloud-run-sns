c0dd6ee8aefacfcbfe71bdbd27149bc22dc3704b
aya-se <kakkeno1@gmail.com>
2023-02-16 19:02:05 +0900

    Delete Dockerfile.development

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -11,12 +11,12 @@ export default NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
+        token.idToken = account.id_token;
       }
       return token;
     },
     async session({ session, token }) {
-      session.user.accessToken = token.accessToken;
+      session.user.idToken = token.idToken;
       return session;
     },
   },
--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -4,12 +4,12 @@ import { JWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
-      accessToken?: string;
+      idToken?: string;
     } & DefaultSession["user"];
   }
 }
 declare module "next-auth/jwt" {
   interface JWT {
-    accessToken?: string;
+    idToken?: string;
   }
 }

be7a0c4afa4ce24b7acba6e130820df0f0118166
aya-se <kakkeno1@gmail.com>
2023-02-19 16:25:59 +0900

    Add Regex schema

--- backend/api/schemas/post.py
+++ backend/api/schemas/post.py
@@ -1,13 +1,12 @@
-from typing import Any, Optional
+from typing import Any
 from pydantic import BaseModel, Field
 
 
 class PostBase(BaseModel):
-    user_name: Optional[str] = Field(None, example="John Doe")
-    user_email: Optional[str] = Field(None, example="example@gmail.com")
-    user_image: Optional[str] = Field(
-        None, example="/google-cloud.png")
-    text: Optional[str] = Field(None, example="Hello World!")
+    user_name: str = Field(..., example="John Doe")
+    user_email: str = Field(..., example="example@gmail.com", regex=r".+@.+\..+", description="Email address")
+    user_image: str = Field(None, example="https://example.com/example", regex=r"https?://.+\..+", description="URL to image")
+    text: str = Field(..., example="Hello World!")
 
 
 class PostCreate(PostBase):
@@ -24,4 +23,4 @@ class PostDelete(BaseModel):
 
 class Post(PostBase):
     id: str
-    timestamp: Any = Field(None, example="2021-01-01T00:00:00.000000Z")
+    timestamp: Any = Field(..., example="2021-01-01T00:00:00.000000Z")

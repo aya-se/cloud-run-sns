4da09ed93f0b9c2361bc257648fd76eee7f38217
aya-se <kakkeno1@gmail.com>
2023-02-24 19:11:36 +0900

    Modify api/posts.ts

--- frontend/src/pages/api/posts.ts
+++ frontend/src/pages/api/posts.ts
@@ -4,31 +4,15 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const API_URL = process.env.API_URL;
-  if (req.method === "POST") {
-    const response = await fetch(`${API_URL}/posts`, {
-      method: "POST",
-      headers: {
-        Authorization: req.headers.authorization || "",
-        "Content-Type": "application/json",
-        accept: "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  }
-  if (req.method === "DELETE") {
-    if (!req.query.id) return;
-    const response = await fetch(`${API_URL}/posts`, {
-      method: "DELETE",
-      headers: {
-        Authorization: req.headers.authorization || "",
-        "Content-Type": "application/json",
-        accept: "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  }
+  const response = await fetch(`${API_URL}/posts`, {
+    method: req.method,
+    headers: {
+      Authorization: req.headers.authorization || "",
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify(req.body),
+  });
+  const data = await response.json();
+  res.status(200).json(data);
 }
